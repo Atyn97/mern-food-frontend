c0ad8b8073a065d35383cf96f931d3b64abcec1b
Atyn97 <fatin.nooraina97@gmail.com>
2024-03-27 11:42:30 +0100

    add search page layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,31 @@ import cook from "../assets/cook.jpg";
 import restaurant from "../assets/restaurant.jpg";
 import rider from "../assets/delivery-guy.jpg";
 import appDownload from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-40">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-40">
         <h1 className="text-5xl font-bold tracking-tight text-green-600">
           Order delivery near you
         </h1>
+
         <span className="text-xl">
           Where should we deliver your food today?
         </span>
+        <SearchBar
+          onSubmit={handleSearchSubmit}
+          placeHolder="Search by City or Town"
+        />
       </div>
       <div className="grid md:grid-cols-3 gap-5">
         <div>
