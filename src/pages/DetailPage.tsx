f523aeba8cad05404e4e038c3c6efbc2773ed8b3
Atyn97 <fatin.nooraina97@gmail.com>
2024-04-02 14:04:53 +0200

    add to cart

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,12 +1,59 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
 import MenuItemInfo from "@/components/MenuItemInfo";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (menuItem: MenuItem) => {
+    setCartItems((prevCartItems) => {
+      // 1. Check if the item is already in the cart
+      const existingCartItem = prevCartItems.find(
+        (cartItems) => cartItems._id === menuItem._id
+      );
+
+      let updatedCartItems;
+
+      // 2. If the item is in the cart, update the quantity
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+
+        // 3. If item is not in the cart, add it as new item
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      return updatedCartItems;
+    });
+  };
 
   if (isLoading || !restaurant) {
     return "Loading...";
@@ -16,7 +63,7 @@ const DetailPage = () => {
     <div className="flex flex-col gap-10">
       <AspectRatio ratio={16 / 5}>
         <img
-          src={restaurant.lastUpdated}
+          src={restaurant.imageUrl}
           className="rounded-md object-cover h-full w-full"
         />
       </AspectRatio>
@@ -25,10 +72,17 @@ const DetailPage = () => {
           <RestaurantInfo restaurant={restaurant} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
           {restaurant.menuItems.map((menuItem) => (
-            <MenuItemInfo menuItem={menuItem} />
+            <MenuItemInfo
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
           ))}
         </div>
-        <div className="flex flex-col gap-4"></div>
+        <div className="flex flex-col gap-4">
+          <Card>
+            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+          </Card>
+        </div>
       </div>
     </div>
   );
